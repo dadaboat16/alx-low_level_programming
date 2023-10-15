f0b30bf0da32cb4cf89270cb09a5c2c6c5dd586b
dadaboat16 <dadaboat16@gmail.com>
2023-10-14 20:28:29 -0700

    Print a line about programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-/* This C program prints a line using the puts function*/
 #include <stdio.h>
-int main()
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
